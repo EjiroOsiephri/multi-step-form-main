869fda00cc52ccef025c3094d9e2a7506db9e714
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-03-26 18:31:35 +0100

    starting of last page

--- src/components/LastPage.jsx
+++ src/components/LastPage.jsx
@@ -5,7 +5,51 @@ import { useNavigate, useLocation } from 'react-router-dom'
 
 const LastPage = () => {
     return (
-        <div>LastPage</div>
+        <div className="container">
+            <div className="first-container">
+                <div className="button-container">
+                    <div className="first-btn">
+                        <button >1</button>
+                        <div className="typography">
+                            <h3 className='first-child'>Step 1</h3>
+                            <h3>Your info
+                            </h3>
+                        </div>
+                    </div>
+
+                    <div className="first-btn">
+                        <button>2</button>
+                        <div className="typography">
+                            <h3 className='first-child'>Step 2</h3>
+                            <h3>Select plan</h3>
+                        </div>
+                    </div>
+
+                    <div className="first-btn">
+                        <button>3</button>
+                        <div className="typography">
+                            <h3 className='first-child'>Step 3</h3>
+                            <h3>Add-ons</h3>
+                        </div>
+                    </div>
+
+                    <div className="first-btn">
+                        <button className='btn-active'>4</button>
+                        <div className="typography">
+                            <h3 className='first-child'>Step 4</h3>
+                            <h3>Summary</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="second">
+
+                <div className="buttons">
+                    <button className='prev-page-btn'><Link to="/thirdpage" >Go Back</Link></button>
+                </div>
+            </div>
+        </div>
+
     )
 }
 
